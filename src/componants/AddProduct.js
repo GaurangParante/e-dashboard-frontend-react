0fdf9bf1d78d7ca780e89086a4a0374cec96f7bc
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-07-17 13:52:43 +0530

    react js validation

--- src/componants/AddProduct.js
+++ src/componants/AddProduct.js
@@ -5,11 +5,16 @@ const AddProduct = () => {
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [company, setCompany] = useState('');
+    const [error, setError] = useState(false)
     const addProduct = async () => {
+
+        if (!name || !price || !category || !company) {
+            setError(true);
+            return false;
+        }
         // console.log(name, price, category, company);
         const userId = JSON.parse(localStorage.getItem('user'))._id;
         // console.log(userID);
-
         const result = await fetch('http://localhost:5000/add-product', {
             method: 'POST',
             body: JSON.stringify({ name, price, category, company, userId }),
@@ -25,9 +30,17 @@ const AddProduct = () => {
         <div className="product">
             <h1>Add Product</h1>
             <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} className="inputBox" placeholder="Enter Product Name" />
+            {error && !name && <span className="invalid-input">Enter valid name</span>}
+
             <input type="text" value={price} onChange={(e) => { setPrice(e.target.value) }} className="inputBox" placeholder="Enter Product Price" />
+            {error && !price && <span className="invalid-input">Enter valid price</span>}
+
             <input type="text" value={category} onChange={(e) => { setCategory(e.target.value) }} className="inputBox" placeholder="Enter Product Category" />
+            {error && !category && <span className="invalid-input">Enter valid category</span>}
+
             <input type="text" value={company} onChange={(e) => { setCompany(e.target.value) }} className="inputBox" placeholder="Enter Product Company" />
+            {error && !company && <span className="invalid-input">Enter valid company</span>}
+
             <button onClick={addProduct} className="appButton" >Add Product</button>
         </div>
     )
